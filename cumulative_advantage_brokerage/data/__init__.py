079152b4054a7591f8c7178b544cbb8c2778bd5e
Jan Bachmann <jan@mannbach.de>
2024-07-12 08:22:45 +0000

    Small fixes

--- cumulative_advantage_brokerage/data/__init__.py
+++ cumulative_advantage_brokerage/data/__init__.py
@@ -1,3 +1,3 @@
 from .transform_to_csv import TransferToCSV
 from .integrate_citation_data import CitationDataIntegrator
-from csv_handling import read_dataframes, write_csv
+from .csv_handling import read_dataframes, write_csv
