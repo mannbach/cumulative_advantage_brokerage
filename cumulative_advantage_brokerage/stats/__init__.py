09971e32397b37c5842580ef6eeb1f96a0cd08f3
Jan Bachmann <bachmann@csh.ac.at>
2024-07-13 13:12:54 +0000

    Export groupers, tests and comparisons

--- cumulative_advantage_brokerage/stats/__init__.py
+++ cumulative_advantage_brokerage/stats/__init__.py
@@ -1,9 +1,10 @@
-from brokerage_comparison import \
-    MetricCollaboratorSeriesBrokerageFrequencyComparison,\
-    MetricCollaboratorSeriesBrokerageRateComparison
-from statistical_tests import\
+from .brokerage_comparison import\
+    CollaboratorSeriesBrokerageComparison,\
+    CollaboratorSeriesRateStageComparison,\
+    CollaboratorSeriesRateStageCorrelation
+from .statistical_tests import\
     StatisticalTest, PearsonPermutTest, SpearmanPermutTest,\
     MannWhitneyPermutTest, KolmogorovSmirnovPermutTest,\
     ContKolmogorovSmirnovPermutTest
-from grouper import Grouper, GrouperDummy, GrouperRole,\
+from .grouper import Grouper, GrouperDummy, GrouperRole,\
     GrouperGender, GrouperBirthDecade
