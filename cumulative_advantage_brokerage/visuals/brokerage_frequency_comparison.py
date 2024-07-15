b29282f2112437dc494b5c57521c8d23fff0fa80
Jan Bachmann <bachmann@csh.ac.at>
2024-07-15 21:39:39 +0000

    Add gender and decade plots

--- cumulative_advantage_brokerage/visuals/brokerage_frequency_comparison.py
+++ cumulative_advantage_brokerage/visuals/brokerage_frequency_comparison.py
@@ -5,8 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from .visuals import draw_zooming_box, draw_zooming_edge, plot_cdfs
-from ..stats import StatisticalTest, GrouperDummy, Grouper, MannWhitneyPermutTest
-from ..constants import OFFSET_MARKERS, N_STAGES, TPL_STR_IMPACT, TPL_CM_IMPACT, CM_CITATION, CM_PRODUCTIVITY, WIDTH_FIG_PAPER, HEIGHT_FIG_PAPER
+from ..stats import StatisticalTest, GrouperDummy, Grouper, MannWhitneyPermutTest, GrouperGender, GrouperBirthDecade
+from ..constants import OFFSET_MARKERS, N_STAGES, TPL_STR_IMPACT, TPL_CM_IMPACT, CM_CITATION, CM_PRODUCTIVITY, WIDTH_FIG_PAPER, HEIGHT_FIG_PAPER, M_GENDER_LABEL
+from ..dbm.models.gender import GENDER_FEMALE, GENDER_MALE, GENDER_UNKNOWN
 
 STAGE_MAX_DIFF_HIGH = 3 # Comparison 3 <-> 4
 STAGE_MAX_DIFF_LOW = 1
@@ -65,7 +66,7 @@ def plot_test_results(
                 if (stage_target_curr == STAGE_MAX_DIFF_HIGH) and (metric == TPL_STR_IMPACT[0]):
                     t_y_example[0] = d_test_sel["test_statistic"].loc[STAGE_EXAMPLE]
                 if (stage_target_curr == STAGE_MAX_DIFF_LOW) and (metric == TPL_STR_IMPACT[1]):
-                    t_y_example[1] = d_test_sel["test_statistic"].loc[STAGE_EXAMPLE]
+                    t_y_example[1] = d_test_sel.loc[STAGE_EXAMPLE, "test_statistic"]
 
                 ax_col.scatter(
                     d_test_sel.index.values + _off,
@@ -86,7 +87,7 @@ def plot_test_results(
             ax_col.spines[["top", "right"]].set_visible(False)
 
     for ax in a_ax[-1]:
-        ax.set_xlabel("career stage $s$")
+        ax.set_xlabel("career stage $s_i$")
         # ticks = ax.get_xticks()
         # ax.set_xticks(ticks=[int(x) for x in ticks])
         ax.set_xticks(ticks=range(N_STAGES-1), labels=[f"$s_{s}$" for s in range(N_STAGES - 1)])
@@ -131,16 +132,15 @@ def plot_brokerage_frequency_comparison(
     plot_cdfs(
         ax=right_top_subplot,
         l_cdf=tpl_a_cdf[0],
-        l_stages=range(N_STAGES - 1),
-        l_labels=[f"$Q_{m} \\leftrightarrow Q_{m+1}$" for m in range(N_STAGES - 1)],
+        l_stages=(STAGE_MAX_DIFF_HIGH, STAGE_MAX_DIFF_HIGH+1),
+        l_labels=[f"$Q_{STAGE_MAX_DIFF_HIGH+i}$" for i in range(2)],
         color_map=CM_CITATION,
-        x_label=None
     )
     plot_cdfs(
         ax=right_bottom_subplot,
         l_cdf=tpl_a_cdf[1],
-        l_stages=range(N_STAGES - 1),
-        l_labels=[f"$Q_{m} \\leftrightarrow Q_{m+1}$" for m in range(N_STAGES - 1)],
+        l_stages=(STAGE_MAX_DIFF_LOW, STAGE_MAX_DIFF_LOW+1),
+        l_labels=[f"$Q_{STAGE_MAX_DIFF_LOW+i}$" for i in range(2)],
         color_map=CM_PRODUCTIVITY,
     )
 
@@ -186,12 +186,171 @@ def plot_brokerage_frequency_comparison(
             bbox_to_anchor=(0.36, pos_y))
         fig.text(.4, pos_y-.02, metric, transform=fig.transFigure)
         l_legends.append(legend)
+    for ax in (right_top_subplot, right_bottom_subplot):
+        legend=ax.legend(frameon=False, handlelength=1, loc="lower right")
+        l_legends.append(legend)
     fig.text(.1, .95, "A", fontdict={"weight": "bold"}, transform=fig.transFigure)
     fig.text(.1, .47, "B", fontdict={"weight": "bold"}, transform=fig.transFigure)
     fig.subplots_adjust(hspace=5.5, wspace=.5)
 
     return fig
-    fig.savefig(
-        os.path.join(
-            FOLDER_OUT,
-            f"cs-{METRIC_STR_CS}_t-{test_choice.label_file}_bin-{ID_METRIC_TESTS_CIT}-{ID_METRIC_TESTS_PRD}_b-freq.pdf"), bbox_extra_artists=l_legends, bbox_inches='tight')
+
+def plot_gender_comparison(
+        tpl_d_tests_bf_cmp_gender
+):
+    fig, a_ax = plt.subplots(
+        figsize=[WIDTH_FIG_PAPER, (3/5)*HEIGHT_FIG_PAPER],
+        nrows=2, ncols=2, sharex=True, sharey=True)
+
+    plot_test_results(
+        a_ax=a_ax,
+        grouper=GrouperGender,
+        stat_test=MannWhitneyPermutTest,
+        tpl_d_test=tpl_d_tests_bf_cmp_gender,
+        show_not_sign=False)
+
+    l_legends = []
+    for metric, cm, pos_y in zip(TPL_STR_IMPACT, TPL_CM_IMPACT, (.97, .53)):
+        legend = fig.legend(
+            [plt.Line2D([], [],
+                color=cm((stage_max+1) / N_STAGES),
+                label=stage_max,
+                marker="o",
+                linestyle="None")\
+                    for stage_max in range(N_STAGES - 1)],
+            [f"$Q_{m} \\leftrightarrow Q_{m+1}$" for m in range(N_STAGES - 1)],
+            frameon=False,
+            ncol=N_STAGES - 1,
+            columnspacing=.1,
+            handletextpad=.05,
+            loc="upper center",
+            bbox_to_anchor=(0.5, pos_y))
+        # legend.set_title(metric)
+        fig.text(.45, pos_y-.02, metric, transform=fig.transFigure)
+        l_legends.append(legend)
+
+    for ax in a_ax[0]:
+        ax.set_title(None)
+    for g, ax in zip((GENDER_FEMALE, GENDER_MALE), a_ax[0,:]):
+        ax.text(.08, .8, M_GENDER_LABEL[g.gender], transform=ax.transAxes)
+
+    fig.subplots_adjust(hspace=.7, wspace=.1)
+    return fig, a_ax, l_legends
+
+def plot_bf_test_results_dec(
+    tpl_fig_ax: Tuple[plt.Figure, List[plt.Axes]],
+    d_test: pd.DataFrame,
+    color_map: str,
+    show_not_sign: bool = False,
+    print_stages: bool = True
+):
+    _, a_ax = tpl_fig_ax
+
+    _gs_tests = d_test.groupby(
+            ["stage", "max_stage_curr","max_stage_next","grouping_key"])\
+        .first()
+
+    for stage, ax_row in enumerate(a_ax):
+        print(f"\tstage: {stage}")
+        ax_row.axhline(
+            MannWhitneyPermutTest.v_neutral,
+            color="black", linestyle="dashed")
+
+        for stage_target_curr, _off in zip(
+                range(N_STAGES - 1),
+                (-(3/2)*OFFSET_MARKERS, -OFFSET_MARKERS/2, OFFSET_MARKERS/2, (3/2)*OFFSET_MARKERS)):
+            print(f"\t\tstage_max: {stage_target_curr}")
+            _color= color_map((stage_target_curr+1) / N_STAGES)
+            _off *= 10
+
+            stage_target_next = stage_target_curr + 1
+
+            _idx = (stage, stage_target_curr, stage_target_next)
+            if _idx not in _gs_tests.index:
+                continue
+
+            d_test_sel = _gs_tests.loc[_idx].sort_index()
+            if not show_not_sign:
+                d_test_sel = d_test_sel[d_test_sel["p_value"] < .05]
+            if len(d_test_sel) == 0:
+                continue
+
+            _alphas= [1. if p <.05 else .3\
+                        for p in d_test_sel["p_value"] if p < 0.05 or show_not_sign]
+
+            ax_row.scatter(
+                [int(dec) * 10 + _off for dec in d_test_sel.index.values],
+                d_test_sel["test_statistic"],
+                color=_color,
+                marker="o",
+                alpha=_alphas)
+            for x,y,l,h,a in zip(
+                    d_test_sel.index.values,
+                    d_test_sel["test_statistic"],
+                    d_test_sel["ci_low"],
+                    d_test_sel["ci_high"],
+                    _alphas):
+                if None in (l,h):
+                    continue
+                ax_row.errorbar(
+                    [int(x) * 10 + _off], [y],
+                    yerr=([np.abs(np.abs(y)-np.abs(l))],[np.abs(np.abs(h)-np.abs(y))]),
+                    color=_color,
+                    alpha=a,
+                    fmt="none")
+
+        ax_row.spines[["top", "right"]].set_visible(False)
+
+    if print_stages:
+        for stage, ax in enumerate(a_ax):
+            ax.text(.075, .9, f"$s_{stage}$", fontsize=10, transform=ax.transAxes)
+    a_ax[-1].set_xlabel("Decade of first publication")
+
+def plot_birth_decade_comparison(
+    tpl_d_tests_bf_cmp: Tuple[pd.DataFrame, pd.DataFrame]
+):
+    fig, a_ax = plt.subplots(
+        nrows=N_STAGES - 1,
+        ncols=2,
+        sharex=True,
+        sharey=True,
+        figsize=(WIDTH_FIG_PAPER, HEIGHT_FIG_PAPER))
+    l_artists = []
+
+    plot_bf_test_results_dec(
+        tpl_fig_ax=(fig,a_ax[:,0]),
+        d_test=tpl_d_tests_bf_cmp[0],
+        color_map=TPL_CM_IMPACT[0],
+    )
+
+    plot_bf_test_results_dec(
+        tpl_fig_ax=(fig,a_ax[:,1]),
+        d_test=tpl_d_tests_bf_cmp[1],
+        color_map=TPL_CM_IMPACT[1],
+        print_stages=False
+    )
+
+    for metric, pos_x, color_map in zip(TPL_STR_IMPACT, (.15, .6), TPL_CM_IMPACT):
+        legend = fig.legend(
+            [plt.Line2D([], [],
+                color=color_map((stage_max+1) / N_STAGES),
+                label=stage_max,
+                marker="o",
+                linestyle="None")\
+                    for stage_max in range(N_STAGES - 1)],
+            [f"$Q_{m} \\leftrightarrow Q_{m+1}$" for m in range(N_STAGES - 1)],
+            frameon=False,
+            ncol=2,
+            columnspacing=.1,
+            handletextpad=.05,
+            loc="center left",
+            bbox_to_anchor=(pos_x, .96),
+            title=metric)
+        legend.get_title().set_fontsize(10)
+        l_artists.append(legend)
+    t=fig.text(.05, .4, MannWhitneyPermutTest.label_y, rotation=90, transform=fig.transFigure)
+    l_artists.append(t)
+
+    fig.subplots_adjust(hspace=.5, wspace=.1)
+
+    return fig, a_ax, l_artists
---
+++ scripts/03_plotting/figure_si_gender_comparison.py
@@ -0,0 +1,73 @@
+import os
+from typing import Dict, Any
+from argparse import ArgumentParser
+
+from cumulative_advantage_brokerage.config import parse_config
+from cumulative_advantage_brokerage.constants import\
+    ARG_POSTGRES_DB_APS, TPL_STR_IMPACT,\
+    STR_CITATIONS, STR_PRODUCTIVITY, STR_CAREER_LENGTH,\
+    ARG_PATH_CONTAINER_OUTPUT
+from cumulative_advantage_brokerage.stats import\
+    MannWhitneyPermutTest,\
+    CollaboratorSeriesBrokerageComparison,\
+    KolmogorovSmirnovPermutTest
+from cumulative_advantage_brokerage.dbm import\
+    PostgreSQLEngine, CumAdvBrokSession, GENDER_FEMALE, GENDER_MALE
+from cumulative_advantage_brokerage.queries import\
+    get_bf_comparison_results_by_id, init_metric_id
+from cumulative_advantage_brokerage.visuals import\
+    plot_gender_comparison
+
+MAP_TESTS = {
+    MannWhitneyPermutTest.label_file: MannWhitneyPermutTest,
+    KolmogorovSmirnovPermutTest.label_file: KolmogorovSmirnovPermutTest
+}
+
+def parse_args() -> Dict[str, Any]:
+    ap = ArgumentParser()
+
+    ap.add_argument("-idcmp-cit", f"--id-comparisons-{STR_CITATIONS}",
+        default=None, type=int)
+    ap.add_argument("-idcmp-prd", f"--id-comparisons-{STR_PRODUCTIVITY}",
+        default=None, type=int)
+
+    d_a = vars(ap.parse_args())
+
+    return d_a
+
+def main():
+    config = parse_config([ARG_POSTGRES_DB_APS])
+    engine = PostgreSQLEngine.from_config(config, key_dbname=ARG_POSTGRES_DB_APS)
+    args = parse_args()
+
+    file_out = os.path.join(
+        config[ARG_PATH_CONTAINER_OUTPUT], "si_brokerage_frequency_gender_comparison.pdf")
+
+    tpl_d_test = []
+    with CumAdvBrokSession(engine) as session:
+        for str_metric,  in zip(
+                TPL_STR_IMPACT):
+            print(f"Working on `{str_metric}`...\nGetting test results.")
+            id_metric_test = args[f"id_comparisons_{str_metric}"]
+
+            if id_metric_test is None:
+                id_metric_test = init_metric_id(
+                    dict(metric=str_metric,
+                         type=CollaboratorSeriesBrokerageComparison.__name__),
+                    session)
+            tpl_d_test.append(get_bf_comparison_results_by_id(
+                    id_metric_config=id_metric_test,
+                    metric=str_metric,
+                    session=session))
+
+    fig, _, l_legends = plot_gender_comparison(
+        tpl_d_tests_bf_cmp_gender=tpl_d_test,
+    )
+    print(f"Saving figure to {file_out}")
+    fig.savefig(
+        file_out,
+        bbox_inches='tight',
+        bbox_extra_artists=l_legends)
+
+if __name__ == "__main__":
+    main()
