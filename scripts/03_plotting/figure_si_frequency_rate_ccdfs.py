4a89412c43e4a7e7174962a0cbd7f57968f1478a
Jan Bachmann <bachmann@csh.ac.at>
2024-07-16 08:21:17 +0000

    Add legend label

--- scripts/03_plotting/figure_si_frequency_rate_ccdfs.py
+++ scripts/03_plotting/figure_si_frequency_rate_ccdfs.py
@@ -148,6 +148,7 @@ def main():
                 ccdf=True,
                 print_ylabel=False
             )
+    fig_br.text(.225, .9085, "career stages")
 
     print(f"Saving frequency CCDFs to {file_out_frequencies}.")
     fig_bf.savefig(file_out_frequencies,
